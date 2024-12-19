9431ffb431316355ce14b69ba58988c5aa13d6ef
artyomgostev <artyomgostev26082005@gmail.com>
2024-12-19 17:10:56 +0300

    Add Codeforces

---
+++ codeforces/A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+
+void solve() {
+    
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test = 1;
+    cin >> test;
+
+    while (test--) {
+        solve();
+        cout << '\n';
+    }
+    return 0;
+}
