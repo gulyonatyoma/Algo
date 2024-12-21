0f19d49d0d18e880a023b229cd1ffbfd2f1c3523
artyomgostev <artyomgostev26082005@gmail.com>
2024-12-21 21:20:56 +0300

    exam_j

---
+++ Algocourses_exam/J.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int long long
+
+void solve() {
+    int n, k; cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test = 1;
+    while (test--) {
+        solve();
+        cout << '\n';
+    }
+
+    return 0;
+}
